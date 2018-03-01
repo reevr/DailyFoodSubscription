1020eb491f4fb31b0e53cf4092db804ea7457795
reevr <reev.r00@gmail.com>
2018-03-01 10:41:12 +0530

    key and price details configuration added

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    GOOGLE_CLIENT_ID: '173782500951-3iafqmja7op75ug1qorrambapioqqlvv.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: 'Y-n8isVtvYWn2T8BnlDUNVNK',
+    GOOGLE_CLIENT_CALLBACK: '/auth/google/callback'
+}
